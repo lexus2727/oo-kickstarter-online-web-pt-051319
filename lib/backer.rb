e99cd54c775be391e815ee63b6a58079617c71b5
Candice Parker <lexus2424@gmail.com>
2019-06-08 17:31:24 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,13 @@
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+end
+
+def backed_project()
+  @backed_project
+  
+end
+end
